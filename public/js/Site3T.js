dd7e23ada3e1465e9456fd45362ed231a643c3cc
H B <harveysiby@gmail.com>
2019-11-15 12:27:01 +0100

    setup project

---
+++ public/js/Site3T.js
@@ -0,0 +1,8 @@
+// var i = 0;
+// function change_image(){
+//   document.getElementById("slideshow").src = "img"+(i++ % 6)+".jpeg";
+// }
+
+// function slider(){
+//   setInterval(change_image, 1000);
+// }
